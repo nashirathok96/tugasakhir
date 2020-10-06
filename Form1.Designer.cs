55d585cec5aee34b10934dcd51d1ce2e0cc76ab1
Taufik Hidayat <taufikdev88@gmail.com>
2020-10-06 21:35:38 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Motor_Position_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.zgc = new ZedGraph.ZedGraphControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputKp = new System.Windows.Forms.TextBox();
+            this.inputKi = new System.Windows.Forms.TextBox();
+            this.inputKd = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.inputRef = new System.Windows.Forms.NumericUpDown();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.btnSetting2 = new System.Windows.Forms.Button();
+            this.btnSetting3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inputRef)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(104, 14);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 21);
+            this.cbPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM PORT: ";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(380, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(26, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "R";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(231, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(143, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // zgc
+            // 
+            this.zgc.Location = new System.Drawing.Point(16, 41);
+            this.zgc.Name = "zgc";
+            this.zgc.ScrollGrace = 0D;
+            this.zgc.ScrollMaxX = 0D;
+            this.zgc.ScrollMaxY = 0D;
+            this.zgc.ScrollMaxY2 = 0D;
+            this.zgc.ScrollMinX = 0D;
+            this.zgc.ScrollMinY = 0D;
+            this.zgc.ScrollMinY2 = 0D;
+            this.zgc.Size = new System.Drawing.Size(563, 397);
+            this.zgc.TabIndex = 4;
+            this.zgc.UseExtendedPrintDialog = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSetting3);
+            this.groupBox1.Controls.Add(this.btnSetting2);
+            this.groupBox1.Controls.Add(this.btnSetting);
+            this.groupBox1.Controls.Add(this.inputKd);
+            this.groupBox1.Controls.Add(this.inputKi);
+            this.groupBox1.Controls.Add(this.inputKp);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(585, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 114);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SETTING";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "KP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "KI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "KD";
+            // 
+            // inputKp
+            // 
+            this.inputKp.Location = new System.Drawing.Point(48, 19);
+            this.inputKp.Name = "inputKp";
+            this.inputKp.Size = new System.Drawing.Size(72, 20);
+            this.inputKp.TabIndex = 5;
+            this.inputKp.Text = "0";
+            // 
+            // inputKi
+            // 
+            this.inputKi.Location = new System.Drawing.Point(48, 49);
+            this.inputKi.Name = "inputKi";
+            this.inputKi.Size = new System.Drawing.Size(72, 20);
+            this.inputKi.TabIndex = 6;
+            this.inputKi.Text = "0";
+            // 
+            // inputKd
+            // 
+            this.inputKd.Location = new System.Drawing.Point(48, 78);
+            this.inputKd.Name = "inputKd";
+            this.inputKd.Size = new System.Drawing.Size(72, 20);
+            this.inputKd.TabIndex = 7;
+            this.inputKd.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnInput);
+            this.groupBox2.Controls.Add(this.inputRef);
+            this.groupBox2.Location = new System.Drawing.Point(585, 161);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 54);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "INPUT";
+            // 
+            // inputRef
+            // 
+            this.inputRef.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.inputRef.Location = new System.Drawing.Point(9, 19);
+            this.inputRef.Maximum = new decimal(new int[] {
+            180,
+            0,
+            0,
+            0});
+            this.inputRef.Name = "inputRef";
+            this.inputRef.Size = new System.Drawing.Size(120, 20);
+            this.inputRef.TabIndex = 0;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInput.Location = new System.Drawing.Point(135, 19);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(54, 23);
+            this.btnInput.TabIndex = 7;
+            this.btnInput.Text = "SET";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting.Location = new System.Drawing.Point(126, 17);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(63, 23);
+            this.btnSetting.TabIndex = 8;
+            this.btnSetting.Text = "SET";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // btnSetting2
+            // 
+            this.btnSetting2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting2.Location = new System.Drawing.Point(126, 46);
+            this.btnSetting2.Name = "btnSetting2";
+            this.btnSetting2.Size = new System.Drawing.Size(63, 23);
+            this.btnSetting2.TabIndex = 9;
+            this.btnSetting2.Text = "SET";
+            this.btnSetting2.UseVisualStyleBackColor = true;
+            this.btnSetting2.Click += new System.EventHandler(this.btnSetting2_Click);
+            // 
+            // btnSetting3
+            // 
+            this.btnSetting3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting3.Location = new System.Drawing.Point(126, 75);
+            this.btnSetting3.Name = "btnSetting3";
+            this.btnSetting3.Size = new System.Drawing.Size(63, 23);
+            this.btnSetting3.TabIndex = 10;
+            this.btnSetting3.Text = "SET";
+            this.btnSetting3.UseVisualStyleBackColor = true;
+            this.btnSetting3.Click += new System.EventHandler(this.btnSetting3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.zgc);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPort);
+            this.Name = "Form1";
+            this.Text = "Motor Position Control PID Based";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.inputRef)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnConnect;
+        private ZedGraph.ZedGraphControl zgc;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.TextBox inputKd;
+        private System.Windows.Forms.TextBox inputKi;
+        private System.Windows.Forms.TextBox inputKp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.NumericUpDown inputRef;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button btnSetting3;
+        private System.Windows.Forms.Button btnSetting2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Motor_Position_Control
+{
+    public partial class Form1 : Form
+    {
+        GraphPane myPane = new GraphPane();
+        PointPairList refPoint = new PointPairList();
+        PointPairList actPoint = new PointPairList();
+        UInt64 n = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void RefreshComPort()
+        {
+            if (serialPort.IsOpen) serialPort.Close();
+
+            cbPort.Items.Clear();
+            cbPort.Text = String.Empty;
+            cbPort.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            RefreshComPort();
+            serialPort.BaudRate = 115200;
+
+            myPane.Title.Text = "PID Setpoint and Response";
+            myPane.XAxis.Title.Text = "Time";
+            myPane.XAxis.Title.Text = "Angle";
+
+            LineItem refCurve = myPane.AddCurve("Ref", refPoint, Color.Red, SymbolType.None);
+            LineItem actCurve = myPane.AddCurve("Act", actPoint, Color.Blue, SymbolType.None);
+
+            zgc.GraphPane = myPane;
+            zgc.AxisChange();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshComPort();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen) serialPort.Close();
+            if (cbPort.Text.Equals(String.Empty))
+            {
+                MessageBox.Show("Select port first before connect");
+                return;
+            }
+
+            serialPort.PortName = cbPort.Text.Trim();
+            serialPort.Open();
+        }
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show("Serial port not connected");
+                return;
+            }
+            serialPort.WriteLine(inputRef.Value.ToString());
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show("Serial port not connected");
+                return;
+            }
+            serialPort.WriteLine("p" + float.Parse(inputKp.Text.Trim()).ToString().Replace(',','.'));
+            Console.WriteLine("p" + float.Parse(inputKp.Text.Trim()).ToString().Replace(',', '.'));
+        }
+
+        private void btnSetting2_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show("Serial port not connected");
+                return;
+            }
+            serialPort.WriteLine("i" + float.Parse(inputKi.Text.Trim()).ToString().Replace(',', '.'));
+            Console.WriteLine("i" + float.Parse(inputKi.Text.Trim()).ToString().Replace(',', '.'));
+        }
+
+        private void btnSetting3_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show("Serial port not connected");
+                return;
+            }
+            serialPort.WriteLine("d" + float.Parse(inputKd.Text.Trim()).ToString().Replace(',', '.'));
+            Console.WriteLine("d" + float.Parse(inputKd.Text.Trim()).ToString().Replace(',', '.'));
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                String[] dataIn = serialPort.ReadLine().Split(',');
+                if (dataIn.Length == 4)
+                {
+                    if (dataIn[0].Contains("h") && dataIn[3].Contains("l"))
+                    {
+                        refPoint.Add(n, double.Parse(dataIn[1]));
+                        actPoint.Add(n, double.Parse(dataIn[2]));
+                        n++;
+                        zgc.AxisChange();
+                        zgc.Invalidate();
+
+                        if(refPoint.Count > 100)
+                        {
+                            refPoint.RemoveAt(0);
+                            actPoint.RemoveAt(0);
+                        }
+                    }
+                }
+            }
+            catch { };
+        }
+    }
+}
